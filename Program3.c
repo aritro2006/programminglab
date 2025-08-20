551128193c4ea920f1140e37a979ebd76da6a916
aritro2006 <aritroghosh2906@gmail.com>
2025-08-20 06:56:30 +0530

    Update Program3.c
    A program in C to finf the sum, difference, product and quotient of two numbers taking input from the user

--- Program3.c
+++ Program3.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,s,d,p,q;
+printf("Enter the first number");
+scanf("%d",&a);
+printf("Enter the second number");
+scanf("%d",&b);
+s=a+b;
+d=a-b;
+p=a*b;
+q=a/b;
+printf("/nThe sum is:%d",s);
+printf("/nThe difference is:%d",d);
+printf("/nThe quotient is:%d",q);
+printf("/nThe producr is :%d",p);
+}
 
