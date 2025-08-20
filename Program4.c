d02580497357d241c8b6dd193eaf60f6fdc2f9d1
aritro2006 <aritroghosh2906@gmail.com>
2025-08-21 00:27:30 +0530

    Update Program4.c
    A program in c to find the diiference of two numbers

--- Program4.c
+++ Program4.c
@@ -1,1 +1,11 @@
-
+#include<stdio.h>
+int main()
+{
+  int a, b, c;
+printf("Enter the first number:");
+scanf("%d",&a);
+printf("Enter the second number:");
+scanf("%d",&b);
+c=a-b;
+printf("The difference is:%d",c);
+}
