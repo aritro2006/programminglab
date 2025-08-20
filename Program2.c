465ee8650959d801db081bafaae2cd129ae3be33
aritro2006 <aritroghosh2906@gmail.com>
2025-08-20 06:38:45 +0530

    Update Program2.c
    A program in C to find the summation of two numbers taking input from the user

--- Program2.c
+++ Program2.c
@@ -1,1 +1,11 @@
-
+#include<stdio.h>
+int main()
+{
+  float num1 ,num2 ,num3;
+printf("Enter the first number");
+scanf("%f",&num1);
+printf("Enter the second number");
+scanf("%f",&num2);
+num3=num1+num2;
+printf("The summation is:%f",num3);
+}
