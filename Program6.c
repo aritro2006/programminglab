ad9ddbb98886cd0b4dd2b3215e32484e8203d3f8
aritro2006 <aritroghosh2906@gmail.com>
2025-09-02 15:25:29 +0530

    Update Program6.c
    Write a C program to check whether Roll 100 is present or NOT using if only

--- Program6.c
+++ Program6.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+int main()
+{
+  int a;
+  printf("Enter your roll number");
+  scanf("%d",&a);
+  if(a==100)
+  {
+  	printf("Present");
+  }
+  else
+  {
+  	printf("Not Present");
+  }
+}
