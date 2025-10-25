e1f766b98a76fec8a463fee0e45a60061da1a464
aritro2006 <aritroghosh2906@gmail.com>
2025-10-25 17:13:27 +0530

    Update program10.c
    To print a solid square pattern made of asterisks

--- program10.c
+++ program10.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    int i,j;
+    for(i=0;i<5;i++)
+    {
+        for(j=5;j>=1;j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
